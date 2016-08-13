3dffd53b16df3113f78e2fafc30cdb5c977d57c6
ibrahimch <pvtlyf@gmail.com>
2016-08-13 16:26:55 -0700

    added some extra features
    getParam_get, getParam_post, getParam_request, get_base_path, get_fragment, get_root_url

--- src/TwigExtension.php
+++ src/TwigExtension.php
@@ -1,3 +1,8 @@
+
+Editing:  
+/home/pokesman/public_html/hms/App/vendor/slim/twig-view/src/TwigExtension.php
+ Encoding:    Re-open Use Code Editor     Close  Save Changes
+
 <?php
 /**
  * Slim Framework (http://slimframework.com)
@@ -35,15 +40,60 @@ class TwigExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFunction('path_for', array($this, 'pathFor')),
-            new \Twig_SimpleFunction('base_url', array($this, 'baseUrl')),
+            new \Twig_SimpleFunction('get_base_url', array($this, 'baseUrl')),
+            new \Twig_SimpleFunction('getParam_get', array($this, 'getParamGet')),
+            new \Twig_SimpleFunction('getParam_post', array($this, 'getParamPost')),
+            new \Twig_SimpleFunction('getParam_request', array($this, 'getParamRequest')),
+            new \Twig_SimpleFunction('get_base_path', array($this, 'getBasePath')),
+            new \Twig_SimpleFunction('get_path', array($this, 'getPath')),
+            new \Twig_SimpleFunction('get_fragment', array($this, 'getFragment')),
+            new \Twig_SimpleFunction('get_root_url', array($this, 'getRootUrl'))
         ];
     }
+
+// Own
+    public function getParamGet($key)
+    {
+        return ($_GET[$key]) ? $_GET[$key] : '';
+    }
+    public function getParamPost($key)
+    {
+        return ($_POST[$key]) ? $_POST[$key] : '';
+    }
+    public function getParamRequest($key)
+    {
+        return ($_REQUEST[$key]) ? $_REQUEST[$key] : '';
+    }
+    public function getBasePath()
+    {
+        if (method_exists($this->uri, 'getBasePath')) {
+            return $this->uri->getBasePath();
+        }
+    }
+    public function getPath()
+    {
+        if (method_exists($this->uri, 'getPath')) {
+            return $this->uri->getPath();
+        }
+    }
+    public function getFragment()
+    {
+        if (method_exists($this->uri, 'getFragment')) {
+            return $this->uri->getFragment();
+        }
+    }
+    public function getRootUrl()
+    {
+        if (method_exists($this->uri, 'getBasePath') && method_exists($this->uri, 'getPath')) {
+            return $this->uri->getBasePath() . DIRECTORY_SEPARATOR . trim($this->uri->getPath(), '/');
+        }
+    }
+// End Own
 
     public function pathFor($name, $data = [], $queryParams = [], $appName = 'default')
     {
         return $this->router->pathFor($name, $data, $queryParams);
     }
-
     public function baseUrl()
     {
         if (is_string($this->uri)) {
@@ -53,7 +103,6 @@ class TwigExtension extends \Twig_Extension
             return $this->uri->getBaseUrl();
         }
     }
-
     /**
      * Set the base url
      *
@@ -65,3 +114,4 @@ class TwigExtension extends \Twig_Extension
         $this->uri = $baseUrl;
     }
 }
+
